5080196be65f36f0f3df54670a705683336722c9
Kirill Liapustin <asaprokki@gmail.com>
2024-11-09 22:31:10 +0500

    Добавлены текстовые функции, заголовочный файл, доработка функции Main

--- Final project C++/Detective.hpp
+++ Final project C++/Detective.hpp
@@ -1,4 +1,28 @@
 #pragma once
 
-void Act1();
+#include<iostream>
 
+// Статы игрока
+extern int assertiveness;
+extern int	diplomacy;
+extern int	observation;
+extern int table;
+extern int window;
+extern int chair;
+
+void Stats();
+void Cabinet();
+void Director();
+void Act1assertivenes();
+void Act1diplomacy();
+void Director2();
+void Director3();
+void Security();
+void Act3assertivenes();
+void Act3diplomacy();
+void Japanese();
+void Accountant();
+void Table();
+void Window();
+void Chair();
+void Exit();
--- Final project C++/Stats.cpp
+++
@@ -1,5 +0,0 @@
-#include "Detective.hpp";
-
-int Assertiveness = 0; // Напористость
-int Diplomacy = 0; // Дипломатичность
-int Observation = 0; // Наблюдательность
